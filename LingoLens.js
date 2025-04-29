bc27bd5a2a6ade751f0dbb304772987c64d4d944
Kenn1toS <maxsafetymk@gmail.com>
2025-04-30 01:47:00 +0300

    first commit

---
+++ LingoLens.js
@@ -0,0 +1,10 @@
+function handleClick() {
+    const button = document.querySelector(".ButtonOpen");
+    const nav = document.querySelector(".nav");
+    const main = document.querySelector("main");
+  
+    button.classList.toggle("active");
+    nav.classList.toggle("none");
+  
+  }
+  alert("Hello");
